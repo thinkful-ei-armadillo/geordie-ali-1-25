c631621510ba3c26b14d1c17beb437d8f36d73e4
Geordie Connell <geordie.connell@gmail.com>
2019-01-26 17:30:33 -0500

    complete breed search, find scooby doo

--- dogbreed.js
+++ dogbreed.js
@@ -1,50 +1,41 @@
-use strict';
+'use strict';
 
 function getDogBreed(breed) {
-    fetch(`https://images.dog.ceo/breeds/${generateBreed(breed)}/images/random`)
+  fetch(`https://dog.ceo/api/breed/${generateBreed(breed)}/images/random`)
     .then(response => response.json())
-    .then(responseJson => { 
-        displayResults(responseJson)
+    .then(responseJson => {
+      displayResults(responseJson);
     })
     .catch(error => alert(error.message));
-  }
 }
 
 function generateBreed(response) {
-    let breedArray = response.split(" "); // [french, bulldog]
-    let finalArray = [];
-    for (let i = 0; i < breedArray.length; i++) {
-        finalArray.append(breedArray.pop()); // finalArray = [bulldog, french]
-    }
-    let breedText = finalArray.join('-');
-    return generatedHtml(breedText);
+  let breedArray = response.split(' '); // [french, bulldog]
+  let finalArray = [];
+  for (let i = 0; i < breedArray.length; i++) {
+    finalArray.push(breedArray.pop()); // finalArray = [bulldog, french]
+  }
+  let breedText = finalArray.join('-');
+  return breedText;
 }
 
-function generateHtml(response) {
-    let htmlText = "";
-
-  for (let i = 0; i < finalArray.length; i++) {
-    htmlText = `<img src="${breedText}" class="default-images">`;
-    }
-  return htmlText;
+function generateHtml(breedText) {
+  return `<img src="${breedText.message}" class="default-images">`;
 }
 
 function displayResults(responseJson) {
-    console.log(responseJson);
-    $('.default-images').html(
-      generateHtml(responseJson));
+  console.log(responseJson);
+  $('.default-images').html(
+    generateHtml(responseJson));
     
-  }
+}
 
 function inputForm() {
   $('form').on('submit', (event => {
-    event.preventDefault();
-    if (!event.target.dogEntry.value) {
-        getDogImages();
-    } else {   
-    getDogImages(parseInt(event.target.dogEntry.value, 10));
-    }
-  }));
+    event.preventDefault();  
+    getDogBreed(event.target.dogEntry.value);
+  }
+  ));
 }
 
 $(function() {
